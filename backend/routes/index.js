60eb005e8396bbbcf768979f6bc47d4c47074c16
Chase Kessler <chasekessler30@yahoo.com>
2021-09-10 12:15:50 -0400

    testing server

---
+++ backend/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+
+//setting cookie on the res with the name of XSRF-TOKEN to the value of req.csrfToken's return val, then sending hello world
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
+
+
+
+
+
+
+module.exports = router;
