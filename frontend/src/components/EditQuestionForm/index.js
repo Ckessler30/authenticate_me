b2b7d6027b322a1b158b52b6e6fefaeaedf7c79d
Chase Kessler <chasekessler30@yahoo.com>
2021-09-15 17:50:08 -0400

    made edit question styling

--- frontend/src/components/EditQuestionForm/index.js
+++ frontend/src/components/EditQuestionForm/index.js
@@ -42,14 +42,29 @@ const EditQuestionForm = ({question}) => {
         <div className="editQuestionForm">
           <div className="editQuestionFormInner">
             <form className="editQuestionInputs" onSubmit={handleSubmit}>
-              <button onClick={() => setShowEditQuestion(false)}>
-                <i class="fas fa-times"></i>
-              </button>
-              <h2>Edit Question</h2>
-              <input type="text" value={title} onChange={(e)=> setTitle(e.target.value) } required/>
+              <div className="editQuestionTop">
+                <button onClick={() => setShowEditQuestion(false)}>
+                  <i class="fas fa-times"></i>
+                </button>
+                <h2>Edit Question</h2>
+                <input
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  required
+                />
+              </div>
               <div className="editQuestionButtons">
-                <button type="button" onClick={handleCancelClick}>Cancel</button>
-                <button type="submit">Save</button>
+                <button
+                  type="button"
+                  onClick={handleCancelClick}
+                  className="update-buttons2"
+                >
+                  Cancel
+                </button>
+                <button type="submit" className="update-buttons" disabled={question.title === title}>
+                  Save
+                </button>
               </div>
             </form>
           </div>
